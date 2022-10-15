241bfda54e450f0407b7f776fee75adf488804bd
maruf <dev.maruf112@gmail.com>
2022-10-15 07:19:40 +0600

    testing first time

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { getAuth } from "firebase/auth";
+import app from "./firebase.init";
+const auth = getAuth(app);
 function App() {
+  const handleForm = (e) => {
+    e.preventDefault();
+    // console.log("I am handled");
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    // console.log(email,password);
+  };
+  const emailOnchange = (e) => {
+    console.log(e.target.value);
+  };
+  const emailOnchangeBlur = (e) => {
+    console.log(e.target.value);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleForm}>
+        <input
+          onBlur={emailOnchangeBlur}
+          // onChange={emailOnchange}
+          type="email"
+          name="email"
+          placeholder="Your email"
+        />
+        <br />
+        <input
+          // onChange={emailOnchange}
+          onBlur={emailOnchangeBlur}
+
+          type="password"
+          name="password"
+          placeholder="Your password"
+        />
+        <br />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
